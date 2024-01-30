bc6c0e2c257c4541639be6dcd933693207886bf2
FlashOnFire <flashonfire.dev@gmail.com>
2024-01-30 12:07:05 +0100

    Image.h

---
+++ src/Image.h
@@ -0,0 +1,30 @@
+#ifndef _IMAGE_H
+#define _IMAGE_H
+
+
+#include "Pixel.h"
+
+class Image {
+private:
+    Pixel tab[];
+    int dimx, dimy;
+public:
+    Image();
+    Image(int dimensionX, int dimensionY);
+    ~Image();
+
+    Pixel& getPix(int x, int y);
+    Pixel getPix(int x, int y) const;
+
+    void setPix(int x, int y, Pixel pixel);
+
+    void dessinerRectangle(int Xmin, int Ymin, int Xmax, int Ymax, Pixel couleur);
+
+    void effacer(Pixel couleur);
+
+    static void testRegression();
+};
+
+
+
+#endif
