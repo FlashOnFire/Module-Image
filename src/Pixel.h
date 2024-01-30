5c07f73e1e929cb88944802001f149d225c3fa88
tristantrad <thibaut.laracine@etu.univ-lyon1.fr>
2024-01-30 12:35:49 +0100

    add doc for Pixel.h

--- src/Pixel.h
+++ src/Pixel.h
@@ -5,9 +5,20 @@
 struct Pixel {
     unsigned char r, g, b;
 
+    /**
+     * @brief Constructeur par défaut de la classe:
+     * initialise le pixel à la couleur noire
+     */
     Pixel() : r(0), g(0), b(0) {
     };
 
+    /**
+     * @brief Constructeur de la classe:
+     * initialise r,g,b avec les paramètres
+     * @param nr : Entier entre 0 et 255
+     * @param ng : Entier entre 0 et 255
+     * @param nb : Entier entre 0 et 255
+     */
     Pixel(unsigned char nr, unsigned char ng, unsigned char nb) : r(nr), g(ng), b(nb) {
     };
 };
