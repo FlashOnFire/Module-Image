9862d302f23e281ef184b77fee7411faf9391550
tristantrad <thibaut.laracine@etu.univ-lyon1.fr>
2024-01-30 12:55:31 +0100

    main

--- src/mainTest.cpp
+++ src/mainTest.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
 #include "Pixel.h"
+#include "Image.h"
 
 int main() {
+    Image::testRegression();
+    return 0;
+    /*
     Pixel p1;
     Pixel p2(10,20,30);
 
     std::cout << (int) p1.r << std::endl;
     std::cout << (int) p2.b << std::endl;
+     */
 }
