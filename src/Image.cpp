67009babc9fc3b95250707cb630b4bd9efa90d9d
Luquin Emmanuel p2202699 <emmanuel.luquin@etu.univ-lyon1.fr>
2024-01-30 12:22:04 +0100

    Image.Cpp : added construcors and destructor

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,2 +1,21 @@
 #include "Image.h"
 
+Image::Image() {
+    dimx = 0;
+    dimy = 0;
+}
+
+Image::Image(int dimensionX, int dimensionY) : dimx(dimensionX), dimy(dimensionY) {
+    tab = new Pixel[dimx*dimy];
+}
+
+Image::~Image() {
+    delete [] tab;
+    tab = nullptr;
+}
+
+void Image::testRegression() {
+    Image testConstructeurDef();
+    int dimensionX = 0, dimensionY = 0;
+    Image testConstructeurArg(dimensionX, dimensionY);
+}
