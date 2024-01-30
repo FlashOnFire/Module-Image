967ba0c390389bcc8854b70525f03572d99f2528
Luquin Emmanuel p2202699 <emmanuel.luquin@etu.univ-lyon1.fr>
2024-01-30 12:38:20 +0100

    Image.Cpp : added dessiner and effacer

--- src/Image.cpp
+++ src/Image.cpp
@@ -6,31 +6,43 @@ Image::Image() {
 }
 
 Image::Image(int dimensionX, int dimensionY) : dimx(dimensionX), dimy(dimensionY) {
-    tab = new Pixel[dimx*dimy];
+    tab = new Pixel[dimx * dimy];
 }
 
 Image::~Image() {
-    delete [] tab;
+    delete[] tab;
     tab = nullptr;
     dimx = 0;
-    dimy =0;
+    dimy = 0;
 }
 
-Pixel& Image::getPix(int x, int y) {
-    return &tab[y*dimx+x];
+Pixel &Image::getPix(int x, int y) {
+    return tab[y * dimx + x];
 }
 
 Pixel Image::getPix(int x, int y) const {
-    return tab[y*dimx+x];
+    return tab[y * dimx + x];
 }
 
 void Image::setPix(int x, int y, Pixel couleur) {
-    tab[y*dimx+x] = couleur;
+    tab[y * dimx + x] = couleur;
+}
+
+void Image::dessinerRectangle(int Xmin, int Ymin, int Xmax, int Ymax, Pixel couleur) {
+    for (int x = Xmin; x < Xmax; x++) {
+        for (int y = Ymin; y < Ymax; y++) {
+            tab[y * dimx + x] = couleur;
+        }
+    }
+}
+
+void Image::effacer(Pixel couleur) {
+    dessinerRectangle(0, 0, dimx, dimy, couleur);
 }
 
 void Image::testRegression() {
-    Image testConstructeurDef();
-    int dimensionX = 0, dimensionY = 0;
+    Image testConstructeurDef;
+    int const dimensionX = 0, dimensionY = 0;
     Image testConstructeurArg(dimensionX, dimensionY);
 
 }
