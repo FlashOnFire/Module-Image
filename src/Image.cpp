97dfc870f574ed1e8977e94e1cd7d6c2dc4eca7d
Luquin Emmanuel p2202699 <emmanuel.luquin@etu.univ-lyon1.fr>
2024-01-30 12:27:42 +0100

    Image.Cpp : added gets and set

--- src/Image.cpp
+++ src/Image.cpp
@@ -12,10 +12,25 @@ Image::Image(int dimensionX, int dimensionY) : dimx(dimensionX), dimy(dimensionY
 Image::~Image() {
     delete [] tab;
     tab = nullptr;
+    dimx = 0;
+    dimy =0;
+}
+
+Pixel& Image::getPix(int x, int y) {
+    return &tab[y*dimx+x];
+}
+
+Pixel Image::getPix(int x, int y) const {
+    return tab[y*dimx+x];
+}
+
+void Image::setPix(int x, int y, Pixel couleur) {
+    tab[y*dimx+x] = couleur;
 }
 
 void Image::testRegression() {
     Image testConstructeurDef();
     int dimensionX = 0, dimensionY = 0;
     Image testConstructeurArg(dimensionX, dimensionY);
+
 }
